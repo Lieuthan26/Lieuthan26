8dd1d7bfe5982dbf1cfbaca6a2b851d4e9f9a08b
Lieuthan26-Dev <142865392+Lieuthan26@users.noreply.github.com>
2023-08-22 11:40:25 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Address Book</title>
+</head>
+<body>
+    <h1>Address Book</h1>
+    <a href="add_contact.php">Add Contact</a>
+    <a href="view_contacts.php">View Contacts</a>
+</body>
+</html>
