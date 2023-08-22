24a0cd4a1e27bf6f7491527566c27d36ce7a351d
Lieuthan26-Dev <142865392+Lieuthan26@users.noreply.github.com>
2023-08-22 11:41:24 +0700

    process_contact.php

---
+++ process_contact.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+
+    // Lưu thông tin liên hệ vào cơ sở dữ liệu hoặc tập tin
+    // (Chú ý: Đây chỉ là một ví dụ đơn giản, không có kết nối cơ sở dữ liệu thực tế)
+
+    echo "Contact added successfully!";
+} else {
+    echo "Invalid request.";
+}
+?>
