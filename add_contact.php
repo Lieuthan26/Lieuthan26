12650a9bc1e3f322048a600091303cb9223627f1
Lieuthan26-Dev <142865392+Lieuthan26@users.noreply.github.com>
2023-08-22 11:41:02 +0700

    add_contact.php

---
+++ add_contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Contact</title>
+</head>
+<body>
+    <h1>Add Contact</h1>
+    <form action="process_contact.php" method="POST">
+        <label for="name">Name:</label>
+        <input type="text" name="name" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
+
+        <label for="phone">Phone:</label>
+        <input type="text" name="phone" required><br>
+
+        <input type="submit" value="Add Contact">
+    </form>
+</body>
+</html>
