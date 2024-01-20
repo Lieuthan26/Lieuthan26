9036395efa7cd04b741fc6e604677aebd654a9ad
Lieuthan26-Dev <142865392+Lieuthan26@users.noreply.github.com>
2024-01-20 15:23:19 +0700

    Update view_contacts.php

--- view_contacts.php
+++ view_contacts.php
@@ -10,6 +10,7 @@
         <!-- (Chú ý: Đây chỉ là một ví dụ đơn giản, không có kết nối cơ sở dữ liệu thực tế) -->
         <li>Name: John Doe | Email: john@example.com | Phone: 123-456-7890</li>
         <li>Name: Jane Smith | Email: jane@example.com | Phone: 987-654-3210</li>
+        <li>Name: Jack Scan | Email: jack@example.com | Phone: 677-655-7410</li>
     </ul>
 </body>
 </html>
